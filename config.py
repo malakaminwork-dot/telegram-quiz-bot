89d732ba09fde65dcec2550d4455da9a1e3399cf
malakaminwork-dot <malakaminwork@gmail.com>
2026-02-09 01:44:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     # سيتم تعيين هذه من متغيرات البيئة
-    BOT_TOKEN = os.getenv('8440625760:AAEW7YOKF7vwXp7tBFci3Pp2tYEp6O9AyCk', '')
+    BOT_TOKEN = os.getenv('8117038726:AAFEWJ8H1JyEZu6OGkwehp6LdE9K057-3FQ', '')
     ADMIN_IDS = list(map(int, os.getenv('8422436251', '').split(','))) if os.getenv('ADMIN_IDS') else []
     DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///quizzes.db')
